32e9f4da9807b13a143eb3cec50d8b5e51f9c3be
ivankitanov <47246947+ivankitanov@users.noreply.github.com>
2019-02-08 17:10:18 +0200

    args+ modules

---
+++ args_example.py
@@ -0,0 +1,12 @@
+#  Example 1
+def over9000(farg, *argv):
+    print("The first one is: "+farg)
+    for x in argv:
+        print("This is another name: "+ x)
+over9000("blabla", "gdksg" "fdh", "Goku") 
+
+from module import module1
+res=module1.add(4,6)
+print(res)
+
+
